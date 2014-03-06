51056028f815ceceaba56ff08667c92ca0228ba5
Michal <sc0ty@nas.lan>
2014-03-06 23:48:35 +0100

    tmux configuration, better instalation script

--- linux-install.sh
+++ linux-install.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
+
+inst () {
+	if [ ! -e "${HOME}/$1" ]
+	then
+		mv -n "${HOME}/$1" "${HOME}/backup/$1"
+	fi
+	ln -sf $2 "${PWD}/$1" "${HOME}/$1" 
+}
+
 mkdir -p "${HOME}/backup"
 
-mv -n "${HOME}/.vim" "${HOME}/backup/.vim"
-mv -n "${HOME}/.vimrc" "${HOME}/backup/.vimrc"
-mv -n "${HOME}/.bashrc" "${HOME}/backup/.bashrc"
-
-ln -sd "${PWD}/.vim/" "${HOME}/" 
-ln -s "${PWD}/.vimrc" "${HOME}/.vimrc" 
-ln -s "${PWD}/.bashrc" "${HOME}/.bashrc" 
+inst .vim
+inst .vimrc -d
+inst .bashrc
+inst .tmux.conf
 

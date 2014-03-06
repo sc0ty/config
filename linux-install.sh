ef324dee4ec192681690cf66c37b40f7b2294856
Michal <sc0ty@nas.lan>
2014-03-06 23:37:04 +0100

    mc config removed

--- linux-install.sh
+++ linux-install.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 mkdir -p "${HOME}/backup"
-mkdir -p "${HOME}/backup/mc"
 
 mv -n "${HOME}/.vim" "${HOME}/backup/.vim"
 mv -n "${HOME}/.vimrc" "${HOME}/backup/.vimrc"
 mv -n "${HOME}/.bashrc" "${HOME}/backup/.bashrc"
-mv -n "${HOME}/.config/mc/ini" "${HOME}/backup/mc/ini"
 
 ln -sd "${PWD}/.vim/" "${HOME}/" 
 ln -s "${PWD}/.vimrc" "${HOME}/.vimrc" 
 ln -s "${PWD}/.bashrc" "${HOME}/.bashrc" 
-ln -s "${PWD}/mc/ini" "${HOME}/.config/mc/ini" 
 

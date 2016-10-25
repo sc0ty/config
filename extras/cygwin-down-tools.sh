c8223b14775823f56b3cb91be52e4bc94dfb3cb0
Michal Szymaniak <m.szymaniak@adbglobal.com>
2016-10-25 12:06:41 +0200

    removed unused files

--- extras/cygwin-down-tools.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source ./../functions.sh
-
-evalcmd mkdir -p "${HOME}/bin"
-evalorexit wget http://www.paralint.com/projects/notifu/dl/notifu-1.6.zip
-evalorexit unzip notifu-1.6.zip notifu.exe -d "${HOME}/bin"
-evalcmd chmod a+x "${HOME}/bin/notifu.exe"
-evalorexit ln -s "${PWD}/../cygwin/notify-send" "${HOME}/bin/notify-send"
-
